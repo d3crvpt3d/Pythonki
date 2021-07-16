01567175479d957b1ab0b9ceae0fed22315449df
D3crvpt3d <st4r.sprayer@gmail.com>
2021-07-16 04:27:55 +0200

    ...

--- owncode.py
+++ owncode.py
@@ -6,3 +6,19 @@ from PIL import Image
 for x in range(5):
     image = Image.open("creeper64_"+str(x)+".png")
     image.show()
+    KI.
+    
+
+class KI:
+    
+    def 
+    
+    X = []
+    X.append()
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
