c7e055e5ab5dbab19ea882523658170e7a104d3d
D3crvpt3d <st4r.sprayer@gmail.com>
2021-07-17 18:14:11 +0200

    did something

--- owncode.py
+++ owncode.py
@@ -2,44 +2,89 @@ import numpy as np
 from PIL import Image
 
 X = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+X1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 #inputs aus bildern in array mit richtgen guess in stelle "0" (0 = linie von links oben nach rechts unten, 1 = mitte oben unten, 2 = ) 1 und 4 und 5 sind "0"
-i = np.random.randint(1, 10)
-if i == 1:
-    j = 0
-if i == 2:
-    j = 1
-if i == 3:
-    j = 9
-if i == 4:
-    j = 0                       #key
-if i == 5:
-    j = 0
-if i == 6:
-    j = 9
-if i == 7:
-    j = 9
-if i == 8:
-    j = 9
-if i == 9:
-    j = 9
-image = Image.open("9pix_"+str(i)+"_["+str(j)+"].png")
+def training(Name_output_model):
+    i = np.random.randint(1, 10)
+    if i == 1:
+        j = 0
+    elif i == 2:
+        j = 1
+    elif i == 3:
+        j = 9
+    elif i == 4:
+        j = 0                       #key
+    elif i == 5:
+        j = 0
+    elif i == 6:
+        j = 9
+    elif i == 7:
+        j = 9
+    elif i == 8:
+        j = 9
+    elif i == 9:
+        j = 9
 
-position = 1
+    image = Image.open("9pix_"+str(i)+"_["+str(j)+"].png")
 
-for y in range(3):
+    position = 1
 
-    for x in range(3):
+    for y in range(3): #fill pixelvalues to array X (X[0] = right guess)
 
-        tmp = image.getpixel((x, y))
-        X[position] = tmp[0]
-        position += 1
+        for x in range(3):
 
-X[0] = j
+            tmp = image.getpixel((x, y))
+            X[position] = tmp[0]
+            position += 1
 
-#forward prop
-#Z1 = W1 * X + b
+    X[0] = j #first position is right guess
 
-print("I: "+str(i)+" J: "+str(j))
-for f in range(10):
-    print("X "+str(f)+": "+str(X[f]))
+
+    print("I: "+str(i)+" J: "+str(j))
+    for f in range(10):
+        print("X"+str(f)+": "+str(X[f]))
+
+    for i in range(9):
+        X1[i] = X[i+1]
+
+X1_length = len(X1) #length of X1
+
+
+
+def Sample(i, Name_input_model):                                  #use own picture
+    image = Image.open("test_"+str(i)+".png")
+
+
+
+
+#init weights and biases
+
+def weights1(X1_length, output_anzahl):
+    W1 = np.random.randn(X1_length, output_anzahl)
+
+def weights2(inputs_anzahl, output_anzahl):
+    W2 = np.random.randn(inputs_anzahl, output_anzahl)
+
+def weights3(inputs_anzahl, output_anzahl):
+    W3 = np.random.randn(inputs_anzahl, output_anzahl)
+
+
+
+class ReLU:
+    def forward(self, input):
+        return np.maximum(0, input)
+
+
+
+class Softmax:
+    def forward(self, input):
+        exp_values = np.exp(input - np.max(input, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        return probabilities
+
+
+#User Interface
+training("Model1") #wie das model heißen soll
+
+Sample(1, "Model1")#welches bild und welches modell genommen werden soll
