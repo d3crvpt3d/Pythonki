06adf4702c5c588659ca49bfb4d6f71d253a6016
D3crvpt3d <st4r.sprayer@gmail.com>
2021-07-18 19:15:41 +0200

    push push push

--- owncode.py
+++ owncode.py
@@ -21,13 +21,25 @@ def sigmoid_derivative(x):
     sd = sigmoid(x)*(1-sigmoid(x))
     return sd
 
+def softmax(vector):
+	e = np.exp(vector)
+	return e / e.sum()
+
 Neurons_h1 = [20]
 Neurons_h2 = [20]
 Neurons_o = [10]
+output = [10]
+runden = 1
 
 #forward prop
-for x in X:
-        Neurons_h1 = np.dot(X[x], W1.T)
+for runde in range(runden):
+    for x in X:
+        for pixel in range(784):
+            Neurons_h1 = sigmoid(np.dot(X[pixel], W1.T) + b1)
+            Neurons_h2 = sigmoid(np.dot(Neurons_h1, W2.T) + b2)
+            Neurons_o = np.dot(Neurons_h2, W3) + b3
+            pixel += 1
+
 
 
 # Show results
@@ -35,5 +47,5 @@ while True:
     index = int(input("Enter a number (0 - 59999): "))
     img = X[index]
     plt.imshow(img.reshape(28, 28), cmap="Greys")
-    plt.title(str(y[index]) + str(Neurons_o[index]))
+    plt.title("Number: "+str(y[index]) +" Guess: "+ str(output[index]))
     plt.show()
