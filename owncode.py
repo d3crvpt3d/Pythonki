0fc3a3223fb9158f605eadad4161c13c5389e162
D3crvpt3d <st4r.sprayer@gmail.com>
2021-07-18 19:42:13 +0200

    push new weil ich dumm bin

--- owncode.py
+++ owncode.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 
 X, y = get_mnist()
-W1 = np.random.uniform(-0.5, 0.5, (784, 1))
-W2 = np.random.uniform(-0.5, 0.5, (20, 1))
-W3 = np.random.uniform(-0.5, 0.5, (20, 1))
+W1 = np.random.uniform(-0.5, 0.5, (784, 20))
+W2 = np.random.uniform(-0.5, 0.5, (20, 20))
+W3 = np.random.uniform(-0.5, 0.5, (20, 10))
 
-b1 = np.zeros((784, 1))
+b1 = np.zeros((20, 1))
 b2 = np.zeros((20, 1))
-b3 = np.zeros((20, 1))
+b3 = np.zeros((10, 1))
 
 def sigmoid(x):
     s = 1/(1+np.exp(-x))
@@ -25,6 +25,12 @@ def softmax(vector):
 	e = np.exp(vector)
 	return e / e.sum()
 
+def ReLU(x):
+    if x < 0:
+        return 0
+    else:
+        return x
+
 Neurons_h1 = [20]
 Neurons_h2 = [20]
 Neurons_o = [10]
@@ -35,9 +41,12 @@ runden = 1
 for runde in range(runden):
     for x in X:
         for pixel in range(784):
-            Neurons_h1 = sigmoid(np.dot(X[pixel], W1.T) + b1)
-            Neurons_h2 = sigmoid(np.dot(Neurons_h1, W2.T) + b2)
-            Neurons_o = np.dot(Neurons_h2, W3) + b3
+            for xx in range(20):
+                Neurons_h1[xx] = sigmoid(np.dot(X[runde], W1.T) + b1[xx])
+            for xx in range(20):
+                Neurons_h2[xx] = sigmoid(np.dot(Neurons_h1, W2.T) +b2[xx])
+            Neurons_o = np.dot(Neurons_h2, W3[pixel]) + b3
+            output = softmax(Neurons_o)
             pixel += 1
 
 
