2ae91f1fc1112b434559560112acda5af4124c74
D3crvpt3d <st4r.sprayer@gmail.com>
2021-07-20 17:17:46 +0200

    dont know how to backprop yet lol

--- owncode.py
+++ owncode.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-X, y = get_mnist()
+X, y = get_mnist() #X[0-5999 pictures, 0-783 pixel in each picture] y[0-59999 pictures, 0-9 real results in array]
 W1 = np.random.uniform(-0.5, 0.5, (20, 784))
 W2 = np.random.uniform(-0.5, 0.5, (20, 20))
 W3 = np.random.uniform(-0.5, 0.5, (10, 20))
@@ -12,6 +11,8 @@ b1 = np.zeros((20))
 b2 = np.zeros((20))
 b3 = np.zeros((10))
 
+
+#activation functions
 def sigmoid(x):
     s = 1/(1+np.exp(-x))
     return s
@@ -20,8 +21,8 @@ def sigmoid_derivative(x):
     sd = sigmoid(x)*(1-sigmoid(x))
     return sd
 
-def softmax(vector):
-	e = np.exp(vector)
+def softmax(x):
+	e = np.exp(x)
 	return e / e.sum()
 
 def ReLU(x):
@@ -30,26 +31,34 @@ def ReLU(x):
     else:
         return x
 
-output = []
 
+#some global variables
+output = []
 runden = 1
 
-#forward prop
+
 for runde in range(runden):
     #for x in X[59990:]:
         xx = 0
+        #forward prop
         Neurons_h1 = sigmoid(np.dot(W1, X[xx]) + b1)
-        print(X[xx].shape)
-        print("lost")
-        print(Neurons_h1.shape)
         Neurons_h2 = sigmoid(np.dot(W2, Neurons_h1) + b2)
-        Neurons_o = np.dot(W3, Neurons_h2) + b3
+        Neurons_o = sigmoid(np.dot(W3, Neurons_h2) + b3)
         output = softmax(Neurons_o)
+
+        #backprop
+        cost = (output - y[xx]) ** 2 #cost of this iteration
+        print("Output: ")
+        print(output)
+        print("Right: ")
+        print(y[xx])
+        print("Cost: ")
+        print(cost)
+        
+
+
+        #count for X
         xx += 1
-        output = softmax(Neurons_o)
-
-#print(output)
-
 
 
 # Show results
@@ -57,6 +66,6 @@ while True:
     index = int(input("Enter a number (0 - 59999): "))
     img = X[index]
     plt.imshow(img.reshape(28, 28), cmap="Greys")
-    plt.title("Number: "+str(y[index]) +" Guess: "+ str(output[index]))
+    plt.title("Number: "+str(y[index].argmax()) +" Guess: "+ str(output.argmax()))
     plt.show()
     break
