07d6454fab7a11356e88750616f7a0c6d38638e5
D3crvpt3d <st4r.sprayer@gmail.com>
2021-08-11 19:17:17 +0200

    a

--- Use model.py
+++ Use model.py
@@ -6,3 +6,13 @@ from tensorflow.keras import layers
 from keras.models import load_model
 
 model = load_model('model.h5')
+
+# A few random samples
+use_samples = [5, 38, 3939, 27389]
+
+# Convert into Numpy array
+samples_to_predict = np.array(use_samples)
+
+# Generate predictions for samples
+predictions = model.predict(samples_to_predict)
+print(predictions)
